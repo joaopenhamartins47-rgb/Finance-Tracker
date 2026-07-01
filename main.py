f99f7937eedee5bf22c23391e55d8a7bff28e612
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-07-01 02:08:52 -0300

    correcting errors

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from models import Base
 from database import engine
 app = FastAPI()
 
-Base.database.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def test():
