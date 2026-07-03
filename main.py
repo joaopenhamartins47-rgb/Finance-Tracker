5b6272b0cb26f50cca9faf2079a128f0fe3a763f
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-07-02 23:50:34 -0300

    including auth router in main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from models import Base
 from database import engine
+from routers import auth
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-async def test():
-    return {"message": "Hello!"}
+
+app.include_router(auth.router)
