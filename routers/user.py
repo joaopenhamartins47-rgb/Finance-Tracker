fd9db0a77d42c6059dd223c744345fb7b5c6b582
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-08-03 16:12:58 -0300

    fixing output model in user router

--- routers/user.py
+++ routers/user.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from core.security import user_dependency
+from models import Users
 from schemas import UserResponse
 from database import db_dependency
 from services.service_user import get_current_user_from_db
@@ -10,5 +11,5 @@ router = APIRouter(
 )
 
 @router.get('/me', response_model=UserResponse)
-async def get_user_info (current_user: user_dependency, db: db_dependency) -> UserResponse:
+async def get_user_info (current_user: user_dependency, db: db_dependency) -> Users:
     return get_current_user_from_db(current_user['username'], db)
